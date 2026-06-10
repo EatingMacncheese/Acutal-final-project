819b4b715c0aa9abcd806c2eac57f5ab45fe5609
339784910 <339784910@2260I-L29-010>
2026-06-10 13:18:36 -0400

    ftyjfut

--- Acutal final project/Game1.cs
+++ Acutal final project/Game1.cs
@@ -20,6 +20,9 @@ namespace Acutal_final_project
         Random generator;
         Texture2D Target;
         Rectangle targetRect;
+        Texture2D pannelTexture, buttonTexture;
+
+        Rectangle pannelRect, buttonRect;
 
         SoundEffect gunShot;
 
@@ -45,7 +48,7 @@ namespace Acutal_final_project
             window = new Rectangle(0, 0, 900, 600);
             mouseRect = new Rectangle(10, 10, 20, 20);
             
-            generator = new Random(10);
+            generator = new Random();
             _graphics.PreferredBackBufferWidth = window.Width;
             _graphics.PreferredBackBufferHeight = window.Height;
             _graphics.ApplyChanges();
@@ -54,11 +57,14 @@ namespace Acutal_final_project
             Speed = new Vector2(generator.Next(-4, 4), generator.Next(-4, 4));
             textFont = Content.Load<SpriteFont>("File");
 
+            pannelRect = new Rectangle(600, 0, 300, 600);
+            buttonRect = new Rectangle(650, 25, 200, 35);
+
             gunShot = Content.Load<SoundEffect>("gunshot");
 
             Score = 0;
 
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < 100; i++)
             {
                 int size = generator.Next(40, 100);
                 targets.Add(new Targets(targetTexture, new Rectangle(generator.Next(_graphics.PreferredBackBufferWidth - size), generator.Next(_graphics.PreferredBackBufferHeight - size), size, size), new Vector2(generator.Next(-4, 4), generator.Next(-4, 4))));
@@ -73,6 +79,8 @@ namespace Acutal_final_project
 
             mouseTexture = Content.Load<Texture2D>("pngwing.com");
             targetTexture = Content.Load<Texture2D>("enemy");
+            pannelTexture = Content.Load<Texture2D>("red");
+            buttonTexture = Content.Load<Texture2D>("dark red");
         }
 
         protected override void Update(GameTime gameTime)
@@ -110,6 +118,8 @@ namespace Acutal_final_project
             if (mouseState.LeftButton == ButtonState.Pressed || prevMouseState.LeftButton == ButtonState.Released)
                 
 
+                // CLICKER ELEMENTS
+
             
 
             // TODO: Add your update logic here
@@ -134,7 +144,8 @@ namespace Acutal_final_project
             _spriteBatch.Draw(mouseTexture, mouseRect, Color.White);
 
             _spriteBatch.DrawString(textFont, "Score: " + Score, new Vector2(10, 10), Color.Black);
-
+            _spriteBatch.Draw(pannelTexture, pannelRect, Color.White);
+            _spriteBatch.Draw(buttonTexture, buttonRect, Color.White);
             _spriteBatch.End();
 
             base.Draw(gameTime);
